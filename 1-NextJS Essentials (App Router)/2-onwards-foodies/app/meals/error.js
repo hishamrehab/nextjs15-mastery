5ad28be5c1304d9a397a32bd6b079b195ac152d3
hishamrehab <hishamrehab30@gmail.com>
2025-11-22 12:56:39 +0200

    Handeling Errors

---
+++ 1-NextJS Essentials (App Router)/2-onwards-foodies/app/meals/error.js
@@ -0,0 +1,9 @@
+"use client";
+export default function Error() {
+    return (
+        <main className="error">
+            <h1>An erro occured!</h1>
+            <p>Failed to fetch meal data. Pleade Tty again later.</p>
+        </main>
+    )
+}
